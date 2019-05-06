457b920ed651fa461f7a24cb1c0b70e76424a49b
alecabreralituma@gmail.com <alecabreralituma@gmail.com>
2019-05-06 14:03:20 +0200

    Configuracion inicial issue #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import {HttpClientModule} from '@angular/common/http';
+import {Routes} from '@angular/router';
+import { PublicationComponent } from './publication/publication.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    FooterComponent,
+    PublicationComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
