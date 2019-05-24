4ace9f47545d01747d49d6da2bde710bd2e9c351
alecabreralituma@gmail.com <alecabreralituma@gmail.com>
2019-05-24 18:49:23 +0200

    refactorgin project issue #10

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -18,7 +18,7 @@ export class ProjectComponent implements OnInit {
   private title: string;
   private description: string;
   private category: string;
-  private specific_objects: string;
+  private key_words: string;
   private initial_date: Date;
   private final_date: Date;
   project: Project;
@@ -40,7 +40,7 @@ export class ProjectComponent implements OnInit {
      this.registerProjectForm = this.formBuilder.group({
       vtitle: ['', [Validators.required]],
       vdescription: ['', [Validators.required]],
-      vspecific_objects: ['', [Validators.required]],
+      vkey_words: ['', [Validators.required]],
       vinitial_date: ['', [Validators.required]],
       vfinal_date: ['', [Validators.required]],
       vcategory: ['', [Validators.required]],
@@ -66,21 +66,21 @@ export class ProjectComponent implements OnInit {
       return;
     } else {
         console.log("datos validos");
-        this.add();
+
     }
 
   }
 
   add() {
+    console.log("add");
     this.project = {title: this.title,
                     description: this.description,
-                    specific_objectives: this.specific_objects,
+                    key_words: this.key_words,
                     initial_date: this.initial_date,
                     final_date: this.final_date,
                     enabled: true,
-                    category: this.category,
                     user_id: Number.parseInt(this.userid, 10)};
-
+    console.log(this.project);
 
     this.projectService.saveProject(this.project).subscribe(response => {
       console.log('regsitro Correcto');
--- src/app/shared/model/project.ts
+++ src/app/shared/model/project.ts
@@ -3,10 +3,9 @@ import {User} from './user';
 export class Project {
   title: string;
   description: string;
-  specific_objectives: string;
+  key_words: string;
   initial_date: Date;
   final_date: Date;
   enabled?: boolean;
-  category: string;
   user_id: number;
 }
